9f7f7ab673f6e626ee23e4e9cb672361c2653ba2
misfir3 <jason@foothillsecurity.com>
2019-06-08 09:50:15 -0600

    Update WebSecurityConfig.java
    oops ... WebGoat, not Webwolf

--- code-review/java/misconfig/WebSecurityConfig.java
+++ code-review/java/misconfig/WebSecurityConfig.java
@@ -28,15 +28,16 @@
  * @version $Id: $Id
  * @since December 12, 2015
  */
-package org.owasp.webwolf;
+package org.owasp.webgoat;
 
 import lombok.AllArgsConstructor;
-import org.owasp.webwolf.user.UserService;
+import org.owasp.webgoat.users.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
@@ -56,19 +57,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry security = http
                 .authorizeRequests()
-                .antMatchers("/css/**", "/images/**", "/js/**", "/fonts/**", "/webjars/**").permitAll()
-                .antMatchers("/WebWolf/**").authenticated()
-                .anyRequest().permitAll();
-        security.and().csrf().disable().formLogin()
-                .loginPage("/login").failureUrl("/login?error=true");
+                .antMatchers("/css/**", "/images/**", "/js/**", "fonts/**", "/plugins/**", "/registration", "/register.mvc").permitAll()
+                .antMatchers("/servlet/AdminServlet/**").hasAnyRole("WEBGOAT_ADMIN", "SERVER_ADMIN") //
+                .antMatchers("/JavaSource/**").hasRole("SERVER_ADMIN") //
+                .anyRequest().authenticated();
         security.and()
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/WebWolf/home", true)
+                .defaultSuccessUrl("/welcome.mvc", true)
+                .usernameParameter("username")
+                .passwordParameter("password")
                 .permitAll();
         security.and()
-                .logout()
-                .permitAll();
+                .logout().deleteCookies("JSESSIONID").invalidateHttpSession(true);
+        security.and().csrf().disable();
+
+        http.headers().cacheControl().disable();
+        http.exceptionHandling().authenticationEntryPoint(new AjaxAuthenticationEntryPoint("/login"));
+    }
+
+    //// TODO: 11/18/2016 make this a little bit more configurabe last part at least
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/plugin_lessons/**", "/XXE/**");
     }
 
     @Autowired
